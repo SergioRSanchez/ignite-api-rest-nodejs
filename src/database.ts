49f2316d5f1b82c530af687ab7383ca13e0eb54c
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-06-25 13:19:39 -0300

    feat: created type for transaction table

--- src/database.ts
+++ src/database.ts
@@ -1,4 +1,4 @@
-import { knex as setupKenx, Knex } from 'knex'
+import { knex as setupKnex, Knex } from 'knex'
 
 import { env } from './env'
 
@@ -14,4 +14,4 @@ export const config: Knex.Config = {
   },
 }
 
-export const knex = setupKenx(config)
+export const knex = setupKnex(config)
